419b91c3682030909438bf7e91f8e0159efe25ab
shajin hari <shajin.hari@vtitan.com>
2024-03-14 08:43:32 +0530

    initial commit

--- app/app/src/main/java/com/shajin/spotify/MainActivity.kt
+++ app/app/src/main/java/com/shajin/spotify/MainActivity.kt
@@ -3,13 +3,6 @@ package com.shajin.spotify
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.shajin.spotify.ui.theme.SpotifyTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,29 +10,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SpotifyTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SpotifyTheme {
-        Greeting("Android")
     }
 }
